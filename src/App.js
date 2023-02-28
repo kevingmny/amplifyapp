231928d685bbb53ec879bac19027809e3cb89ee4
kevingmny <you@example.comkevingmny23@gmail.com>
2023-02-28 16:26:12 +0100

    added auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, Button} from '@aws-amplify/ui-react'
 
-function App() {
+function App({ signOut}) {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>We now have Auth!</h1>
+        <Button onClick={signOut}>Sign out</Button>
       </header>
+      
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
